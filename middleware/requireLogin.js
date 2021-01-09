9f6137b7db52c8e505921cec0d556bc7af2ca4ad
Najeeb Ullah <68231281+najeeb-ullah@users.noreply.github.com>
2021-01-09 16:26:36 +0500

    Update requireLogin.js

--- middleware/requireLogin.js
+++ middleware/requireLogin.js
@@ -10,7 +10,7 @@ module.exports = (req, res, next) => {
     return res.status(401).json({ error: "you must be logged in" });
   }
   const token = authorization.replace("Bearer ", "");
-  console.log(token);
+  console.log(token[0]);
   jwt.verify(token, JWT_SECRET, (err, payload) => {
     if (err) {
       return res.status(401).json({ error: "you must be signed in" });
