b115abc1bd94e5b97cb8dda3eb9a4e17068e6ec3
Najeeb Ullah <68231281+najeeb-ullah@users.noreply.github.com>
2021-01-09 16:29:49 +0500

    Update requireLogin.js

--- middleware/requireLogin.js
+++ middleware/requireLogin.js
@@ -5,12 +5,14 @@ const User = mongoose.model("User");
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
+  console.log(authorization);
 
   if (!authorization) {
     return res.status(401).json({ error: "you must be logged in" });
   }
   const token = authorization.replace("Bearer ", "");
   console.log(token[0]);
+  console.log(token[1]):
   jwt.verify(token, JWT_SECRET, (err, payload) => {
     if (err) {
       return res.status(401).json({ error: "you must be signed in" });
